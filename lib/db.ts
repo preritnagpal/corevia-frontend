d911590190be0ad27ab323852012253bd5b8aba2
Prerit Nagpal <137195896+preritnagpal@users.noreply.github.com>
2025-12-30 15:53:32 +0530

    Delete lib directory

--- lib/db.ts
+++
@@ -1,7 +0,0 @@
-// Frontend/lib/db.ts
-import mongoose from "mongoose";
-
-export async function connectDB() {
-  if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(process.env.MONGODB_URI!);
-}
